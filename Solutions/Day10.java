06adae06fb0b89f3c08c1d09eae1247a82dbefd9
Alexander Karlsson <alex94@comhem.se>
2015-12-17 21:29:13 +0100

    Optimized solution for day 10

--- Solutions/Day10.java
+++ Solutions/Day10.java
@@ -1,13 +1,12 @@
 /**
- * Solution to day one of Advent of Code 2015
- * Code is very badly optimized, it is possible to finish part two of the problem but it will take hours
+ * Solution to day ten of Advent of Code 2015
  */
 public class Day10 {
     public static void main(String args[]){
         String input = "1113122113";
         for(int i = 0; i<50; i++){
             input = getNewString(input);
-            if(i == 39){
+            if(i + 1 == 40){
                 System.out.println("Answer part one = " + input.length());
             }
         }
@@ -15,33 +14,16 @@ public class Day10 {
     }
 
     public static String getNewString(String oldString){
-        StringBuilder newString = new StringBuilder();
-        StringBuilder tempString = new StringBuilder(oldString);
-        while(!(tempString.length()==0)){
-            amountAndChar(newString,tempString);
+        StringBuilder newString = new StringBuilder("");
+        for(int i = 0; i<oldString.length(); i++){
+            int currentCount = 1;
+            char currentChar = oldString.charAt(i);
+            while(i + 1 < oldString.length() && currentChar == oldString.charAt(i + 1)){
+                i++;
+                currentCount++;
+            }
+            newString.append("" + currentCount + currentChar);
         }
         return newString.toString();
-    }
-
-    public static void amountAndChar(StringBuilder buildString, StringBuilder deleteString){
-        if(deleteString.length()==0){
-            return;
-        }
-        char thisChar = deleteString.charAt(0);
-        int oldLength = deleteString.length();
-        removeItems(deleteString);
-        int amount = oldLength - deleteString.length();
-        buildString.append("" + amount + thisChar);
-    }
-
-    public static void removeItems(StringBuilder oldString){
-        char tempChar = oldString.charAt(0);
-        while(!(oldString.length()==0)){
-            if(oldString.charAt(0)==tempChar){
-                oldString.deleteCharAt(0);
-            }else{
-                return;
-            }
-        }
     }
 }
